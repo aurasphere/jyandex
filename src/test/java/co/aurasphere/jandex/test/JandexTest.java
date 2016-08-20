519e5dca7fed39216e9f7299cda4183b1ac9f8d2
Donato Rimenti <donatohan.rimenti@gmail.com>
2016-08-21 00:59:19 +0200

    Fixed example.

--- src/test/java/co/aurasphere/jandex/test/JandexTest.java
+++ src/test/java/co/aurasphere/jandex/test/JandexTest.java
@@ -1,29 +1,30 @@
 /**
-* MIT License
-* 
-* Copyright (c) 2016 Donato Rimenti
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * MIT License
+ * 
+ * Copyright (c) 2016 Donato Rimenti
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package co.aurasphere.jandex.test;
 
 import java.util.HashMap;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -60,8 +61,13 @@ public class JandexTest {
 		SupportedLanguageResponse response = client.supportedLanguages();
 		HashMap<String, String> langs = response.getLangs();
 		System.out.println("Available languages:");
-		for (String s : langs.keySet()) {
-			System.out.println(s + " : " + langs.get(s));
+		if (langs != null) {
+			Set<String> keys = langs.keySet();
+			if (keys != null) {
+				for (String s : keys) {
+					System.out.println(s + " : " + langs.get(s));
+				}
+			}
 		}
 	}
 
@@ -76,8 +82,10 @@ public class JandexTest {
 		String[] translatedText = response.getText();
 
 		System.out.println("Translated text:");
-		for (String t : translatedText) {
-			System.out.println(t);
+		if (translatedText != null) {
+			for (String t : translatedText) {
+				System.out.println(t);
+			}
 		}
 	}
 }
